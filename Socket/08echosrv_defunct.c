67a823241771857bec4a18c6f0dd13f7510080ee
feb <zpk@192.168.187.130>
2024-05-13 17:52:12 -0700

    POC-0 defunct

---
+++ Socket/08echosrv_defunct.c
@@ -0,0 +1,215 @@
+/*
+服务器多进程，父进程接收连接，返回一个连接给子进程管理，然后父进程继续接受连接。
+出现僵尸进程：
+当客户端断开连接后，服务器管理这个连接的子进程退出，如果父进程没有处理SIGCHLD，这个子进程就会变成僵尸进程。
+解决：
+signal(SIGCHLD, SIG_IGN); 告诉内核不关系子进程的退出，内核会自动回收。
+signal(SIGCHLD, handle_sigchld); 绑定回调处理函数，可以用wait查询子进程退出状态
+
+用wait解决僵尸进程依然可能有问题，如果有多个子进程同时退出，wait不能等待所有子进程的退出，因为它等待第一个子进程就返回了。
+这里应该跟收到几个SIGCHLD没关系，可能好几次处理都在第一个返回了。
+解决：
+用waitpid
+*/
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#define ERR_EXIT(m) \
+        do \
+        { \
+                perror(m); \
+                exit(EXIT_FAILURE); \
+        } while(0)
+
+ssize_t readn(int fd, void *buf, size_t count)
+{
+	size_t nleft = count;
+	ssize_t nread;
+	char *bufp = (char*)buf;
+
+	while (nleft > 0)
+	{
+		if ((nread = read(fd, bufp, nleft)) < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		else if (nread == 0)
+			return count - nleft;
+
+		bufp += nread;
+		nleft -= nread;
+	}
+
+	return count;
+}
+
+ssize_t writen(int fd, const void *buf, size_t count)
+{
+	size_t nleft = count;
+	ssize_t nwritten;
+	char *bufp = (char*)buf;
+
+	while (nleft > 0)
+	{
+		if ((nwritten = write(fd, bufp, nleft)) < 0)
+		{
+			if (errno == EINTR)
+				continue;
+			return -1;
+		}
+		else if (nwritten == 0)
+			continue;
+
+		bufp += nwritten;
+		nleft -= nwritten;
+	}
+
+	return count;
+}
+
+ssize_t recv_peek(int sockfd, void *buf, size_t len)
+{
+	while (1)
+	{
+		int ret = recv(sockfd, buf, len, MSG_PEEK);
+		if (ret == -1 && errno == EINTR)
+			continue;
+		return ret;
+	}
+}
+
+ssize_t readline(int sockfd, void *buf, size_t maxline)
+{
+	int ret;
+	int nread;
+	char *bufp = buf;
+	int nleft = maxline;
+	while (1)
+	{
+		ret = recv_peek(sockfd, bufp, nleft);
+		if (ret < 0)
+			return ret;
+		else if (ret == 0)
+			return ret;
+
+		nread = ret;
+		int i;
+		for (i=0; i<nread; i++)
+		{
+			if (bufp[i] == '\n')
+			{
+				ret = readn(sockfd, bufp, i+1);
+				if (ret != i+1)
+					exit(EXIT_FAILURE);
+
+				return ret;
+			}
+		}
+
+		if (nread > nleft)
+			exit(EXIT_FAILURE);
+
+		nleft -= nread;
+		ret = readn(sockfd, bufp, nread);
+		if (ret != nread)
+			exit(EXIT_FAILURE);
+
+		bufp += nread;
+	}
+
+	return -1;
+}
+
+void echo_srv(int conn)
+{
+	char recvbuf[1024];
+        while (1)
+        {
+                memset(recvbuf, 0, sizeof(recvbuf));
+                int ret = readline(conn, recvbuf, 1024);
+		if (ret == -1)
+			ERR_EXIT("readline");
+		if (ret == 0)
+		{
+			printf("client close\n");
+			break;
+		}
+		
+                fputs(recvbuf, stdout);
+                writen(conn, recvbuf, strlen(recvbuf));
+        }
+}
+
+void handle_sigchld(int sig)
+{
+/*	wait(NULL);*/      //这里不关心更多状态，所以传了NULL
+	while (waitpid(-1, NULL, WNOHANG) > 0)
+		;
+}
+
+int main(void)
+{
+/*	signal(SIGCHLD, SIG_IGN);*/
+	signal(SIGCHLD, handle_sigchld);
+	int listenfd;
+	if ((listenfd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+/*	if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)*/
+		ERR_EXIT("socket");
+
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(5188);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	/*servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");*/
+	/*inet_aton("127.0.0.1", &servaddr.sin_addr);*/
+
+	int on = 1;
+	if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on)) < 0)
+		ERR_EXIT("setsockopt");
+
+	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+		ERR_EXIT("bind");
+	if (listen(listenfd, SOMAXCONN) < 0)
+		ERR_EXIT("listen");
+
+	struct sockaddr_in peeraddr;
+	socklen_t peerlen = sizeof(peeraddr);
+	int conn;
+
+	pid_t pid;
+	while (1)
+	{
+		if ((conn = accept(listenfd, (struct sockaddr*)&peeraddr, &peerlen)) < 0)
+			ERR_EXIT("accept");
+
+		printf("ip=%s port=%d\n", inet_ntoa(peeraddr.sin_addr), ntohs(peeraddr.sin_port));
+
+		pid = fork();
+		if (pid == -1)
+			ERR_EXIT("fork");
+		if (pid == 0)
+		{
+			close(listenfd);
+			echo_srv(conn);
+			exit(EXIT_SUCCESS);
+		}
+		else
+			close(conn);
+	}
+	
+	return 0;
+}
